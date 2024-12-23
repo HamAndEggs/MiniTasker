059ee30b9fbee10e3e76c7309657eb077d7f4a33
Richard <16312877+HamAndEggs@users.noreply.github.com>
2024-12-23 11:29:50 +0000

    Moved update script back to using cmake

--- update-service.sh
+++ update-service.sh
@@ -12,13 +12,13 @@ cd ..
 ./update-resources.sh
 
 echo "Building application"
-appbuild
-#./makeit.sh DRM Release
+#appbuild
+./makeit.sh DRM Release
 
 echo "Updaing service"
 sudo systemctl stop mini-tasker
-sudo cp ./bin/release/mini-tasker /usr/bin/mini-tasker
-#sudo cp ./build/Release/mini-tasker /usr/bin/mini-tasker
+#sudo cp ./bin/release/mini-tasker /usr/bin/mini-tasker
+sudo cp ./build/Release/mini-tasker /usr/bin/mini-tasker
 sudo systemctl start mini-tasker
 sudo systemctl status mini-tasker
 
